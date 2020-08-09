aca170597c129b1841272fae16457e0af73a80f5
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-08-09 16:27:44 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            people: []
+        }
+    }
+
+    handlePeople(peopleFetched) {
+        this.setState({
+            people: peopleFetched
+        })
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(data => this.handlePeople(data.people))
+    }
+
+
+    render() {
+        return (
+            <div>
+            {this.state.people.map(person => person.name)}
+            </div>
+        )
+    }
+}
